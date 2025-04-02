a8594ffd60a5657b1c29629945da142fa3188b48
lakshminarayana-dev <nidadalanarayana285@gmail.com>
2025-04-02 23:57:07 +0530

    feastly initial setup

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+    {/*9299a6bd1d0e4e78*/}
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className={'font-poppins-thin'}>Welcome Feastly</Text>
+    </View>
+  );
+}
